ed5d36490522cb09ae16f1ffd40b3015ea8e862d
DawnDevelop <32778430+DawnDevelop@users.noreply.github.com>
2023-02-18 23:29:26 +0100

    added new Validators

---
+++ Samples/SampleNET7.Application/Messages/Commands/CreateWeatherForecastCommandValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace SampleNET7.Application.Messages.Commands;
+
+public class CreateWeatherForecastCommandValidator : AbstractValidator<CreateWeatherForecastCommand>
+{
+  public CreateWeatherForecastCommandValidator()
+  {
+        
+	RuleFor(t => t.Summaries).NotEmpty().WithMessage("Summaries Can not be Empty"); 
+
+	RuleFor(t => t.TemperatureC).NotEmpty().WithMessage("TemperatureC Can not be Empty"); 
+
+
+  }
+}
