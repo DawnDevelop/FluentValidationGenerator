4ec89330d0f1ceac6e6f1531f91647ffaac5171e
DawnDevelop <32778430+DawnDevelop@users.noreply.github.com>
2023-02-19 03:49:35 +0100

    added first iteration of unit tests

---
+++ tests/FluentValidationGenerator.Tests/Utils/Commands/ReflectionTestCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace FluentValidationGenerator.Tests.Utils.Commands;
+
+public class ReflectionTestCommand : IRequest
+{
+    public string? PropertyName { get; set; }
+
+    public int TestInteger { get; set; }
+}
---
+++ tests/FluentValidationGenerator.Tests/Utils/ReflectionHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using FluentValidationGenerator.Tests.Utils.Commands;
+using FluentValidationGenerator.Utils;
+
+namespace FluentValidationGenerator.Tests.Utils;
+
+[TestFixture]
+public class ReflectionHelperTests : BaseTest
+{
+    public static string ReflectionHelperTestsDirectory => ProjectDirectory + "\\Utils";
+
+
+    [Test]
+    public void FindAllCommandsInAssemblyTest()
+    {
+        ReflectionHelper.FindAllCommandsInAssembly(CurrentAssembly).Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public void CreateModelsFromType_TestCommand()
+    {
+        var commandClasses = ReflectionHelper.FindAllCommandsInAssembly(CurrentAssembly);
+        var filteredClasses = commandClasses.Where(x => x.Name == nameof(ReflectionTestCommand)).First();
+
+        Type[] arr = { filteredClasses };
+        var models = ReflectionHelper.CreateModelsFromTypes(arr);
+        
+        models.Should().NotBeNullOrEmpty();
+        models.First().ClassName.Should().Be(nameof(ReflectionTestCommand));
+        models.First().NameSpace.Should().Be(typeof(ReflectionTestCommand).Namespace);
+        models.First().Properties.Should().NotBeNullOrEmpty();
+        models.First().Properties.Where(x => x.Name == nameof(ReflectionTestCommand.PropertyName)).First().Nullable.Should().BeTrue();
+        
+        models.First().Properties.Where(x => x.Name == nameof(ReflectionTestCommand.TestInteger)).First().Nullable.Should().BeFalse();
+
+    }
+}
