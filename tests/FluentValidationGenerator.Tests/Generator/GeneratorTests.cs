0192d191a875167b945efd65753b4133dacef6fc
DawnDevelop <32778430+DawnDevelop@users.noreply.github.com>
2023-02-19 03:55:33 +0100

    Temporarily removed Generator UnitTests

--- tests/FluentValidationGenerator.Tests/Generator/GeneratorTests.cs
+++ tests/FluentValidationGenerator.Tests/Generator/GeneratorTests.cs
@@ -12,31 +12,33 @@ public class GeneratorTests : BaseTest
 {
     public static string GeneratorTestsDirectory => ProjectDirectory + "\\Generator";
     public static string fileName => $"{GeneratorTestsDirectory}\\Commands\\{nameof(GeneratorTestCommand)}Validator.cs";
+    
+    //TODO
 
-    [Test]
-    public void Generator_Should_Create_One_ValidationClass()
-    {
-        var generator = new FluentValidationGenerator.Generator(CurrentAssembly, GeneratorTestsDirectory);
+    //[Test]
+    //public void Generator_Should_Create_One_ValidationClass()
+    //{
+    //    var generator = new FluentValidationGenerator.Generator(CurrentAssembly, GeneratorTestsDirectory);
 
-        generator.GenerateValidators().Should().BeTrue();
+    //    generator.GenerateValidators().Should().BeTrue();
 
-        File.Exists(fileName).Should().BeTrue();
+    //    File.Exists(fileName).Should().BeTrue();
 
-        var fileContent = File.ReadAllText(fileName);
-        fileContent.Should().NotBeNullOrEmpty();
-        fileContent.Should().Contain($"RuleFor(t => t.{nameof(GeneratorTestCommand.PropertyName)})");
-        fileContent.Should().Contain($"RuleFor(t => t.{nameof(GeneratorTestCommand.TestInteger)}).NotEmpty()");
-    }
+    //    var fileContent = File.ReadAllText(fileName);
+    //    fileContent.Should().NotBeNullOrEmpty();
+    //    fileContent.Should().Contain($"RuleFor(t => t.{nameof(GeneratorTestCommand.PropertyName)})");
+    //    fileContent.Should().Contain($"RuleFor(t => t.{nameof(GeneratorTestCommand.TestInteger)}).NotEmpty()");
+    //}
 
-    [TearDown]
-    public void TearDown()
-    {
-        //Delete Created Validators
-        foreach (var item in Directory.EnumerateFiles(GeneratorTestsDirectory, "*.cs", SearchOption.AllDirectories)
-            .Where(file => file.Contains($"Validator.cs")))
-        {
-            File.Delete(item);
-        }
+    //[TearDown]
+    //public void TearDown()
+    //{
+    //    //Delete Created Validators
+    //    foreach (var item in Directory.EnumerateFiles(GeneratorTestsDirectory, "*.cs", SearchOption.AllDirectories)
+    //        .Where(file => file.Contains($"Validator.cs")))
+    //    {
+    //        File.Delete(item);
+    //    }
         
-    }
+    //}
 }
